94aa5712a348f4062d950caa47da6524a33beffb
eth0net <eth0net@pm.me>
2024-12-03 16:38:53 +0000

    init repo

---
+++ 1.5/Source/RakazielPsycastsMod.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace RakazielPsycasts;
+
+/// <summary>
+///     RakazielPsycasts class to load up the mod and initialise everything.
+/// </summary>
+public class RakazielPsycasts : Mod
+{
+    /// <summary>
+    ///     RakazielPsycasts settings reference.
+    /// </summary>
+    internal static RakazielPsycastsSettings settings;
+
+    /// <summary>
+    ///     RakazielPsycasts constructor to load the mod and settings.
+    ///     Also applies patches using harmony.
+    /// </summary>
+    public RakazielPsycasts(ModContentPack content) : base(content)
+    {
+        settings = GetSettings<RakazielPsycastsSettings>();
+        new Harmony("eth0net.RakazielPsycasts").PatchAll();
+    }
+
+    /// <summary>
+    ///     DoSettingsWindowContents configures the settings window.
+    /// </summary>
+    /// <param name="inRect"></param>
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        Listing_Standard listing = new();
+
+        listing.Begin(inRect);
+
+        listing.GapLine();
+        listing.Label("Example Settings");
+        listing.GapLine();
+
+        listing.CheckboxLabeled("Example Setting", ref RakazielPsycastsSettings.exampleSetting, "Example setting description");
+
+        listing.End();
+
+        base.DoSettingsWindowContents(inRect);
+    }
+
+    /// <summary>
+    ///     SettingsCategory returns the name of the settings category.
+    /// </summary>
+    /// <returns></returns>
+    public override string SettingsCategory()
+    {
+        return "RakazielPsycasts".Translate();
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Rakaziel Psycasts
+
+New psycasts for the Vanilla Psycasts Expanded mod.
+
+ALPHA VERSION - NOT FULLY TESTED
+
+Commissioned in the RimWorld Mod Market.
+
+<!--
+## Features
+
+Adds more features to the game:
+
+- First feature
+- Second feature
+- Third feature
+
+## Existing Saves
+
+DISREGARD THIS FOR ALPHA VERSION
+
+This mod should be safe to add or remove from saves.
+-->
+
+## Attributions
+
+- Mod title image generated at https://www.textstudio.com/
+
+## Bugs and Requests
+
+Let me know if there are any bugs or requests, I'll happily do my best to fix them up!
+
+Open to contributions and suggestions!
